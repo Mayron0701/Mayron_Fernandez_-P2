63184c2986dbf6204ab1d8d4767417b233eb3e59
MU15 <>
2021-10-13 16:39:57 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int num1=0; 
+int num2=0; 
+int result=0;
+int main() {
+  cout <<"ponga un numero\n";
+  cin>>num1;
+cout << "ponga un segundo numero\n";
+cin>>num2;
+
+result=num1+num2;
+
+
+
+cout<<"el resultado de la resta "<<num1<<" - "<<num2<<" es:"<<result<<"\n";  
+
+result=num1-num2;
+
+cout<<"el resultado de la suma "<<num1<<" + "<<num2<<" es:"<<result<<"\n";
+}
